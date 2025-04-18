f5a81537c669239df00be0918dabef6b0e93719c
Finn <finnscott1234@gmail.com>
2025-04-18 21:57:17 +0100

    completed sumAll
    finished the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (first, last) {
+  if (!Number.isInteger(first) || !Number.isInteger(last)) return "ERROR";
+  if (first < 0 || last < 0) return "ERROR";
+  if (typeof first != "number" || typeof last != "number") return "ERROR";
+  if (first > last) {
+    const temp = first;
+    first = last;
+    last = temp;
+  }
 
+  let i = first - 1;
+  let finalSum = 0;
+  while (i < last) {
+    i++;
+    finalSum += i;
+  }
+  return finalSum;
 };
 
 // Do not edit below this line
